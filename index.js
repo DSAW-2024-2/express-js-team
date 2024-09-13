73e3fdf670a7bbd12b598285e97df0e51a420245
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-09-12 23:09:23 -0500

    Second student and a restriction for letters were added

--- index.js
+++ index.js
@@ -11,16 +11,29 @@ const students = {
       email: 'gabrielabepe@unisabana.edu.co',
       id: '0000316356'
     },
+    2: {
+      name: 'Nicolas',
+      lastName: 'Clavijo',
+      email: 'nicolasclos@unisabana.edu.co',
+      id: '0000314037'
+    }
   };
+
+
 
   app.get('/user-info/:id', (req, res) => {
     const id = req.params.id;
     
+    if (isNaN(id)) {
+      return res.status(400).json({ error: 'Invalid id' });
+    }
+
     if (students[id]) {
       res.json(students[id]);
     } else {
       res.status(404).json({ error: 'Student not found' });
     }
+
   });
 
   app.listen(port, () => {
